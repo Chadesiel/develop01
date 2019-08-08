20287296692b33b2105765198be73adb69f9b1b6
Lucifer <1663408479@qq.com>
2019-08-08 17:45:18 +0800

    项目基础环境搭建，提交github

---
+++ develope01_stage01/src/main/java/cn/ityuan/develop01/stage01/ReadyEnvironment.java
@@ -0,0 +1,25 @@
+package cn.ityuan.develop01.stage01;
+
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * 1、搭建环境
+ * 2、搭建GitHub仓库
+ *
+ */
+public class ReadyEnvironment {
+
+   static Logger logger = LoggerFactory.getLogger(ReadyEnvironment.class);
+
+    @Test
+    public void test(){
+        System.out.println("简单的环境搭建，测试----------------");
+    }
+
+
+}
+
